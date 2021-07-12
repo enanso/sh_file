81c439bb0f1bb10a10ea2c8645d847f6c8e2b775
漆家佳 <lanmemory@163.com>
2021-07-13 01:52:03 +0800

    SSH连接脚本修改

--- 越狱/connect_phone.sh
+++ 越狱/connect_phone.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-#----------------------越狱手机连接SSH服务----------------------
-# 功能1：连接SSH服务
-# 功能2：自动创建rsa钥匙对儿
+#----------------------越狱手机连接SSH服务免密登录----------------------
+# 功能1：自动创建rsa钥匙对
+# 功能2：免密登录连接SSH服务
+# 功能3：起别名连接登录（配置NICK_NAME值，自动创建配置文件，别名重复自增）
+# 操作2种方法：1.配置IPHONE_LOCAL_IP地址、别名NICK_NAME；
+#            2.终端执行：sh connect_phone.sh 192.168.1.116 nickname
 # 作者 ：JABase
 #-----------------------------------------------------------------------
 
@@ -25,7 +28,10 @@ NC="\033[0m" # No Color
 dir=$(cd `dirname $0`; pwd)
 
 # iPhone连接网络的IP地址
-IPHONE_LOCAL_IP="10.0..246"
+IPHONE_LOCAL_IP="192.168.1.109"
+
+# 服务别名
+NICK_NAME="server"
 
 # SSH【secure shell】登录密码（默认值）
 ALPINE="alpine"
@@ -38,6 +44,34 @@ function inputValue(){
     fi
 }
 
+# 循环输入直到有值为止
+function newValue(){
+    # config文件中是否已经存IPHONE_LOCAL_IP
+    if [[ -z $(filehasword "$1" "$2") ]]; then
+        echo "$2"
+    else
+        # 将$2中的“-”替换成“_”,并拆分成数组
+        arr=($(echo ${2//-/_} | sed 's/_/ /g'))
+        # 数量大于1处理
+        if [[ ${#arr[@]} > 1 ]];then
+            FINAL=${arr[${#arr[@]}-1]}
+            let FINAL++
+            arr[${#arr[@]}-1]=${FINAL}
+            for i in ${arr[*]}
+            do
+                if [[ -n $temp ]]; then
+                    temp="${temp}-${i}"
+                else
+                    temp=${i}
+                fi
+            done
+        else
+            temp="$2-1"
+        fi
+        # 再次处理查询
+        newValue "$1" "$temp"
+    fi
+}
 # 是否包含某文件
 function hasfile(){
     
@@ -81,12 +115,26 @@ function read_dir(){
         fi
     done
 }
+# 判断文件中是否包含某内容
+function filehasword(){
+
+    if cat "$1" | grep "$2" > /dev/null
+    then
+        echo "$1中已存在$2"
+        continue
+    fi
+}
 # 脚本主入口
 function entrance(){
+    # 非空取值$1作为IP地址
     if [[ -n $1 ]];then
         IPHONE_LOCAL_IP=$1
     fi
-
+    # 非空取值$2作为别名
+    if [[ -n $2 ]];then
+        NICK_NAME=$2
+    fi
+    
     if [[ -z $IPHONE_LOCAL_IP ]];then
        #执行循环输入
        inputValue "手机连接网络IP地址"
@@ -110,7 +158,7 @@ function entrance(){
         read_dir "~/.ssh"
         else
         echo "===${RED} 当前没有私钥${NC}"
-        # 1. 生成私钥(ssh-keygen -t rsa -C "your.email@example.com" 可省略直接使用ssh-keygen，密码可以自定义或者直接回车)
+        # 1. ssh-keygen生成私钥(ssh-keygen -t rsa -C "your.email@example.com" 可省略直接使用ssh-keygen，密码可以自定义或者直接回车)
         echo "\n======${GREEN} ‘Enter file in which to save the key (${HOME}/.ssh/id_rsa):’${NC}直接回车，生成id_rsa文件，输入会生成私钥xxx和公钥xxx.pub"
         echo "\n======${GREEN} ‘Enter passphrase (empty for no passphrase):直接回车表示无密码生成rsa文件’${NC}"
         ssh-keygen
@@ -119,19 +167,41 @@ function entrance(){
         
         # 3. 清除旧的公钥信息
         #ssh-keygen -R "$IPHONE_LOCAL_IP"
-
-        # 3.1 自动创建拷贝id_rsa.pub给SSH服务器
-        # ssh-copy-id root@"$IPHONE_LOCAL_IP"
     
-        # 3.1 将id_rsa.pub中的内容拷贝到 authorized_keys中
-        cat id_rsa.pub >> authorized_keys
+#        # 3.1 将id_rsa.pub中的内容拷贝到 authorized_keys中
+#        cat id_rsa.pub >> authorized_keys
         
         # 设置authorized_keys权限
         chmod 600 authorized_keys
         # 设置.ssh目录权限
         chmod 700 -R .ssh
     fi
-        
+    
+    # 3.1 自动创建拷贝id_rsa.pub给SSH远程服务器(第一次连接需要输入alpine)
+    ssh-copy-id root@"$IPHONE_LOCAL_IP"
+
+   # 起别名设置
+   if [[ -n $NICK_NAME ]];then
+        # 如果配置文件不存在，创建配置文件（服务主机起别名使用）
+        if [[ ! -f "${HOME}/.ssh/config" ]];then
+          touch "${HOME}/.ssh/config"
+        fi
+        # config文件中是否已经存IPHONE_LOCAL_IP
+        if [[ -z $(filehasword "${HOME}/.ssh/config" "$IPHONE_LOCAL_IP") ]]; then
+            # 去重操作
+            NICK_NAME=$(newValue "${HOME}/.ssh/config" "$NICK_NAME")
+
+            # > 为覆盖内容 >>为追加内容
+            echo "Host $NICK_NAME">>"${HOME}/.ssh/config"
+            echo "Hostname $IPHONE_LOCAL_IP">>"${HOME}/.ssh/config"
+            echo "User root">>"${HOME}/.ssh/config"
+            
+            cat "${HOME}/.ssh/config"
+        else
+            echo "config文件中包含IP: $IPHONE_LOCAL_IP"
+        fi
+   fi
+
     echo "\n================== ${GREEN}已存在远程服务主机${NC} ==================\n"
     # 读取已经存在远程主机配置
     cat "${HOME}/.ssh/known_hosts"
@@ -141,15 +211,18 @@ function entrance(){
     echo "======${RED} 2.如果出现'ssh: connect to host $IPHONE_LOCAL_IP port 22: Operation timed out'，请检查手机和Mac是否处于同一网络环境中${NC}"
         
     echo "\n======${RED} 连接成功后，输入exit或者ctrl+D退出登录(logout)${NC}"
-    echo "======${RED} 待出现'root@$IPHONE_LOCAL_IP's password:'后，输入ssh默认登录password：${GREEN}${ALPINE}${NC}"
-    # 链接手机
-    ssh root@"$IPHONE_LOCAL_IP"
-#    # 判断指定是否出错
-#    if [ $? -ne 0 ]; then
-#
-#    fi
+    echo "======${RED} 第一次出现'root@$IPHONE_LOCAL_IP's password:'后，输入ssh默认登录password：${GREEN}${ALPINE}${NC}"
+
+    # 使用别名登录
+    ssh $NICK_NAME
+    
+    # 判断指定是否出错
+    if [ $? -ne 0 ]; then
+        # 别名登录报错时，使用IP登录
+        ssh root@"$IPHONE_LOCAL_IP"
+    fi
 
 }
 
-# 脚本启动入口
-entrance $1
+# 脚本启动入口：$1作为IP地址、$2作为别名
+entrance $1 $2
